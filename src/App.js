6d130770d6145e61d41e83faca020334a60d908e
Kimdown <kdowndan@gmail.com>
2024-02-08 11:24:55 +0900

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          11Edit <code>src/App.js</code> and save to reload.
+          1122Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
