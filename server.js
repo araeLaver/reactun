33708071f771296bc6bd72f13ff41052992f1a35
Down <araelaver@gmail.com>
2024-07-21 12:29:46 +0900

    update

--- server.js
+++ server.js
@@ -336,6 +336,6 @@ function getCurrentDrawNumber() {
   const currentWeekSaturday = new Date(baseDate.getTime() + weeksPassed * oneWeekMilliseconds);
   const hasPassedThisWeeksDraw = now > currentWeekSaturday;
   //const currentDrawNumber = baseDrawNumber + (hasPassedThisWeeksDraw ? 1 : 0);
-  const currentDrawNumber = 1129;
+  const currentDrawNumber = 1130;
   return currentDrawNumber; // 
 }
